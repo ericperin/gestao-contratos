f6558ab99a2039979c4a9cf17ee74aebd56f9c19
Eric Perin <eric.perin@outlook.com>
2019-09-07 15:30:27 -0400

    Added contracts controller by scaffolder

---
+++ src/ContractManager.Web/Controllers/ContractsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContractManager.Data;
+using ContractManager.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ContractManager.Web.Controllers
+{
+    [Authorize]
+    public class ContractsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContractsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contracts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Contracts.ToListAsync());
+        }
+
+        // GET: Contracts/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contracts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return View(contract);
+        }
+
+        // GET: Contracts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contracts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClientName,Type,QuantityTraded,NegotiatedValue,StartedAt,Duration,File,CreatedBy,Id,DeletedAt,CreatedAt")] Contract contract)
+        {
+            if (ModelState.IsValid)
+            {
+                contract.Id = Guid.NewGuid();
+                _context.Add(contract);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contract);
+        }
+
+        // GET: Contracts/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contracts.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+            return View(contract);
+        }
+
+        // POST: Contracts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ClientName,Type,QuantityTraded,NegotiatedValue,StartedAt,Duration,File,CreatedBy,Id,DeletedAt,CreatedAt")] Contract contract)
+        {
+            if (id != contract.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contract);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContractExists(contract.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contract);
+        }
+
+        // GET: Contracts/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contracts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return View(contract);
+        }
+
+        // POST: Contracts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var contract = await _context.Contracts.FindAsync(id);
+            _context.Contracts.Remove(contract);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContractExists(Guid id)
+        {
+            return _context.Contracts.Any(e => e.Id == id);
+        }
+    }
+}
