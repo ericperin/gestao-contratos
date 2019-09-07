6b52a33422fd7a11aea648e0bc8a184da94a4011
Eric Perin <eric.perin@outlook.com>
2019-09-07 14:52:00 -0400

    Added domain and web project

---
+++ src/ContractManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContractManager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
